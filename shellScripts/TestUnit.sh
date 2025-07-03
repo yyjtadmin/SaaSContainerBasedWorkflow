670d9375e555005445a65498a0438bc274e35fd0
yyjtadmin <yyjtadmn.industry@siemens.com>
2025-07-03 13:40:40 +0530

    updated the test cases

--- shellScripts/TestUnit.sh
+++ shellScripts/TestUnit.sh
@@ -10,7 +10,7 @@ fi
 
 UNIT_PATH=$1
 export SPLM_LICENSE_SERVER=29000@pnnxflex3:29000@pnnxflex4 
-/usr/site/devop_tools/bin/unit run ${UNIT_PATH} devtest runtest NXTranslators.rep:TranslatorWorker.set -p 6
+/usr/site/devop_tools/bin/unit run ${UNIT_PATH} devtest runtest NXTranslators.rep:NXJT_TranslatorWorker.set -p 6
 casesFailed=`grep "Number of tests:" ${UNIT_PATH}/dt/runs/devtestLastRunFails.txt | cut -d ":" -f2 | tr -d " "`
 if [ $casesFailed != 0 ]
 then
