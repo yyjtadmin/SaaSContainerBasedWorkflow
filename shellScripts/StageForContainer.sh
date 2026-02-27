05ff8bc0ca59248ebbc1377fa8b71b7628802a54
yyjtadmin <yyjtadmn.industry@siemens.com>
2026-02-27 11:29:38 +0530

    remove caejt

--- shellScripts/StageForContainer.sh
+++ shellScripts/StageForContainer.sh
@@ -34,11 +34,9 @@ CONFIG_FILE_VIS=${STAGE_DIR}/tessUG_vis.config
 RUN_UGTOPV_MULTICAD=${STAGE_DIR}/run_ugtopv_multicad
 RUN_UGTOPV_VIS=${STAGE_DIR}/run_ugtopv_vis
 RUN_NXTOJT=${STAGE_DIR}/run_nxtojt
-RUN_CAETOJT=${STAGE_DIR}/run_caetojt
 
 cp -f ${CUSTOMER_ARTIFACTS_DIR}/run_ugtopv_multicad ${RUN_UGTOPV_MULTICAD} || { exit 1;}
 cp -f ${CUSTOMER_ARTIFACTS_DIR}/run_ugtopv_vis ${RUN_UGTOPV_VIS} || { exit 1;}
-cp -f ${CUSTOMER_ARTIFACTS_DIR}/run_caetojt ${RUN_CAETOJT} || { exit 1;}
 cp -f ${CUSTOMER_ARTIFACTS_DIR}/run_nxtojt ${RUN_NXTOJT} || { exit 1;}
 cp -f ${CUSTOMER_ARTIFACTS_DIR}/tessUG_multicad.config ${CONFIG_FILE_MULTICAD} || { exit 1;}
 cp -f ${CUSTOMER_ARTIFACTS_DIR}/tessUG_vis.config ${CONFIG_FILE_VIS} || { exit 1;}
@@ -49,7 +47,6 @@ chmod 0755 ${CONFIG_FILE_VIS} || { exit 1;}
 chmod 0755 ${RUN_UGTOPV_MULTICAD} || { exit 1;}
 chmod 0755 ${RUN_UGTOPV_VIS} || { exit 1;}
 chmod 0755 ${RUN_NXTOJT} || { exit 1;}
-chmod 0755 ${RUN_CAETOJT} || { exit 1;}
 
 # IP 10.146.116.128 is a flex licenser server IP
 # echo "ENV SPLM_LICENSE_SERVER=29000@10.146.116.128" >> ${STAGE_DIR}/dockerfile 
@@ -62,7 +59,6 @@ echo "COPY run_ugtopv_vis         /app/run_ugtopv_vis" >> ${STAGE_DIR}/dockerfil
 echo "COPY tessUG_vis.config      /app/tessUG_vis.config" >> ${STAGE_DIR}/dockerfile
 echo "COPY run_ugtopv_multicad         /app/run_ugtopv_multicad" >> ${STAGE_DIR}/dockerfile
 echo "COPY run_nxtojt         /app/run_nxtojt" >> ${STAGE_DIR}/dockerfile
-echo "COPY run_caetojt         /app/run_caetojt" >> ${STAGE_DIR}/dockerfile
 echo "COPY tessUG_multicad.config      /app/tessUG_multicad.config" >> ${STAGE_DIR}/dockerfile
 echo " " >> ${STAGE_DIR}/dockerfile
 
--- shellScripts/TestUnit.sh
+++ shellScripts/TestUnit.sh
@@ -24,21 +24,3 @@ then
 else
 	echo "No test case failed. So skipping re-run of devtest."
 fi
-
-mv "${UNIT_PATH}/dt" "${UNIT_PATH}/dt_nxjt"
-mv "${UNIT_PATH}/athena" "${UNIT_PATH}/athena_nxjt"
-
-/usr/site/devop_tools/bin/unit run ${UNIT_PATH} devtest runtest cae.rep:CaeJtSaas_Case0001.seq -keep -local
-casesFailed=`grep "Number of tests:" ${UNIT_PATH}/dt/runs/devtestLastRunFails.txt | cut -d ":" -f2 | tr -d " "`
-if [ $casesFailed != 0 ]
-then
-	/usr/site/devop_tools/bin/unit run ${UNIT_PATH} devtest runfails -p 6 -keep -local -parent
-	casesFailed=`grep "Number of tests:" ${UNIT_PATH}/dt/runs/devtestLastRunFails.txt | cut -d ":" -f2 | tr -d " "`
-	if [ $casesFailed != 0 ]
-	then
-		echo "Number of auto test cases failed for CaeJT= $casesFailed...Exiting with error."
-		exit 1
-	fi
-else
-	echo "No test case failed. So skipping re-run of devtest."
-fi
